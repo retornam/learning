7e780a413853281ee253b8e268e5345c7918eaa5
Keala Lusk <contactkeala@gmail.com>
2015-09-01 12:06:03 -0700

    Set cookies, remove cookies, count # of times someone has come to the site using a cookie

--- cookie_example/index.js
+++ cookie_example/index.js
@@ -4,10 +4,26 @@ var app = express();
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+var cookieCount = 1;
 app.get("/", function (req, res) {
-	res.send("I LOVE COOKIES");
+	res.cookie("TimesVisited:", cookieCount)
+	res.send("Hello World");
+	cookieCount++;
+    });
+
+var cookieCount = 0;
+app.get("/cookiemonster", function (req, res) {
+	res.cookie("wdi", 21);
+	res.send("Come to my 127.0.0.1 and I’ll give you sudo access");
+    });
+
+app.get("/clearcookie", function (req, res) {
+		res.cookie("wdi", 21);
+		res.clearCookie("wdi", 21);
+
+        res.send("Is your name Wi-fi? Because I'm really feeling a connection.");
     });
 
 app.listen(3000, function () {
-	console.log("Now running another sexy app on port 3000");
+	console.log("Are you a computer keyboard? Because you're my type;)");
     });
