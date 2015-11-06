cc2de7ca23dde97754e015f1045bd91d418217f7
Keala Lusk <contactkeala@gmail.com>
2015-11-06 12:11:41 -0800

    this%20is%20a%20test

--- ctci/1.4_replace_with_percent.rb
+++ ctci/1.4_replace_with_percent.rb
@@ -1,10 +1,14 @@
 def replace_with_percent (str)
+  new_string = "" 
   str.each_char do |c|
     if c == " "
       c.replace "%20"
-      puts c # prints 3 %20s 
+      new_string = new_string + c
+    else
+      new_string = new_string + c
     end
   end
+  return new_string
 end
 
 replace_with_percent("This is a test")
