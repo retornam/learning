ca9b24f25ae18481785c9e1772d2ec013a17c5bc
Keala Lusk <contactkeala@gmail.com>
2015-11-06 12:00:25 -0800

    came up with correct answer, woot woot

--- ctci/1.3_permutation.rb
+++ ctci/1.3_permutation.rb
@@ -1,5 +1,10 @@
-def all_permutations(str)
-  puts str.chars.to_a.permutation.map(&:join)
+# Given two strings, write a method to decide if one is a permutation of the other
+
+def is_a_permutation? (str1, str2)
+  new_string_array = str1.chars.permutation.map(&:join)
+  new_string_array.each do |s|
+    return true if s == str2
+  end
 end
 
-all_permutations("abc")
+is_a_permutation?("abc", "bca")
