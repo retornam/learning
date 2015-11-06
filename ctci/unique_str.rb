5eeed5cc4cceefecfe1b1e8b6d1f31b86439fabf
Keala Lusk <contactkeala@gmail.com>
2015-11-05 16:30:18 -0800

    added unique string checker

---
+++ ctci/unique_str.rb
@@ -0,0 +1,13 @@
+# Implement an algorithm to determine if a string has all unique characters
+
+def is_str_unique (str)
+  characters = {}
+
+  str.each_char do |c|
+    return false if characters.has_key? c
+    characters[c] = true
+    end
+  end
+
+# this will return false
+is_str_unique("Keala")
