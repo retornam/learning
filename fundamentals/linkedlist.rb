a38b2f028f7262f524c67fb4d8ff934e7fce6d39
Keala Lusk <contactkeala@gmail.com>
2015-11-07 23:07:54 -0800

    linked list

--- fundamentals/linkedlist.rb
+++ fundamentals/linkedlist.rb
@@ -5,4 +5,10 @@ class Node
   def initialize(node)
     @node = node
   end
+
+  def self.node_list(node, msg = nil)
+    msg ||= ""
+    return msg[0..-4] if node.nil?
+    node_list(node.next, msg << "#{node.node} -> ")
+  end
 end
