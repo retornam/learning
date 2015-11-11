aecd024175d11905d97a7cc2e6ace0cfa1f2b9ab
Keala Lusk <contactkeala@gmail.com>
2015-11-11 13:23:21 -0800

    wrote method skeleton

--- ctci/chapter-1/1_6.rb
+++ ctci/chapter-1/1_6.rb
@@ -1,1 +1,4 @@
 # Given an image represented by an NxN matrix, where each px is the img in 4 bytes, write amethod to rotate the img by 90 degrees. Can you do this in place?
+
+def rotate_image
+end 
