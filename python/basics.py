46068f3f1c70d52fc25520ef0a7f13c4e8571081
Keala Lusk <contactkeala@gmail.com>
2015-11-22 17:02:41 -0600

    added directory for python

---
+++ python/basics.py
@@ -0,0 +1,12 @@
+my_name = "Keala"
+
+# length method
+print len(my_name)
+# to lowercase method
+print my_name.lower()
+# capitalize method
+print my_name.upper()
+
+i_love_pi = 3.14
+
+print str(i_love_pi)
