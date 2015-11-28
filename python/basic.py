1dbd4340fc68b3f934952b73c8ff72e959963672
Keala Lusk <contactkeala@gmail.com>
2015-11-27 20:14:56 -0600

    removed prints for debugging

--- python/basic.py
+++ python/basic.py
@@ -1,30 +1,30 @@
 my_name = "Keala"
 
 # length method
-print len(my_name)
+return len(my_name)
 # to lowercase method
-print my_name.lower()
+return my_name.lower()
 # capitalize method
-print my_name.upper()
+return my_name.upper()
 
 i_love_pi = 3.14
 
-print str(i_love_pi)
+return str(i_love_pi)
 
 def hello_world():
-  print "Hello World!"
+  return "Hello World!"
 
 hello_world()
 
 x = 14
 y = 15
 
-if x < y: print x
-elif y < x: print y
-else: print "that is the same number!"
+if x < y: return x
+elif y < x: return y
+else: return "that is the same number!"
 
 x = 16
 ans = 0
-while ans * ans <= x:
+while ans * ans < x:
   ans = ans + 1
 print ans
