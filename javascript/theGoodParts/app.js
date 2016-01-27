bdc9a8d98ccae0ec367da6e1533e8ef7b9794c97
Keala Lusk <contactkeala@gmail.com>
2016-01-27 12:42:14 -0800

    object literals

--- javascript/theGoodParts/app.js
+++ javascript/theGoodParts/app.js
@@ -5,3 +5,31 @@ Function.prototype.method = function (name, func) {
   this.prototype[name] = func;
   return this;
 };
+
+var empty_obj = {};
+
+var stooge = {
+  "first-name": "Jerome",
+  "last-name": "Howard"
+};
+
+var flight = {
+  airline: "Oceanic",
+  number: 825,
+  departure: {
+    IATA: "SYD",
+    time: "2004-09-22 14:55",
+    city: "Sydney"
+  },
+  arrival: {
+    IATA: "LAX",
+    time: "2004-09-23 10:42",
+    city: "Los Angelos"
+  }
+};
+
+stooge["first-name"] // "Jerome"
+flight.departure.IATA // "SYD"
+
+var middle = stooge["middle-name"] || "(none)";
+
