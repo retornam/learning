69289363f88c5ec02eb803a12ff5d273965bc9cc
Keala Lusk <contactkeala@gmail.com>
2016-01-29 21:19:33 -0800

    hanoi

--- javascript/theGoodParts/app.js
+++ javascript/theGoodParts/app.js
@@ -33,3 +33,14 @@ flight.departure.IATA // "SYD"
 
 var middle = stooge["middle-name"] || "(none)";
 // objects are passed around by reference, that are never copied
+
+// recursion
+var hanoi = function hanoi(disc, src, aux, dst) {
+  if (disc > 0) {
+    hanoi(disc - 1, src, dst, aux);
+    document.writeln('Move disc ' + disc + ' from ' + src + ' to ' + dst);
+    hanoi(disc - 1, aux, src, dst);
+  }
+};
+
+hanoi(3, 'Src', 'Aux', 'Dst');
