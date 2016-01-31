5ba796202bf76c67dea2676570739b0c9d916170
Keala Lusk <contactkeala@gmail.com>
2016-01-30 20:09:20 -0800

    prototypal inheritance

--- javascript/theGoodParts/app.js
+++ javascript/theGoodParts/app.js
@@ -81,3 +81,31 @@ var fibonacci = (function () {
 }());
 
 document.writeln(fibonacci(1476));
+
+// prototypal inheritance
+var myMammal = {
+  name : 'Herb the Mammal',
+  get_name : function () {
+    return this.name;
+  },
+  says : function () {
+    return this.saying || '';
+  }
+};
+
+var myCat = Object.create(myMammal);
+myCat.name = 'Henrietta';
+myCat.saying = 'meow';
+nyCat.purr = function (n) {
+  var i, s = '';
+  for (i = 0; i < n; i++) {
+    if (s) {
+      s += '-';
+    }
+    s += 'r';
+  }
+  return s;
+};
+myCat.get_name = function () {
+  return this,says() + ' ' + this.name + ' ' + this.says();
+};
