1cbea7c62fa973767ecdcc68e856facf8b29e670
Keala Lusk <contactkeala@gmail.com>
2016-01-30 16:08:07 -0800

    cascade

--- javascript/theGoodParts/app.js
+++ javascript/theGoodParts/app.js
@@ -44,3 +44,25 @@ var hanoi = function hanoi(disc, src, aux, dst) {
 };
 
 hanoi(3, 'Src', 'Aux', 'Dst');
+
+// cascade
+getElement('myBoxDiv')
+  .move(350, 150)
+  .width(100)
+  .height(100)
+  .color('red')
+  .border('10px outset')
+  .padding('4px')
+  .appendText("Please stand by")
+  .on('mousedown', function (m) {
+    this.startDrag(m, this.getNinth(m));
+  })
+  .on('mousemove', 'drag')
+  .on('mouseup', 'stopDrag')
+  .later(2000, function () {
+    this
+      .color('yellow')
+      .setHTML("What hath God wraught?")
+      .slide(400, 40, 200, 200);
+    })
+  tip('This box is resizeable');
