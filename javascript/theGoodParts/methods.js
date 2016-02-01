c48bea5a25e5a8ceb1c5147676959a1e6e2dcda3
Keala Lusk <contactkeala@gmail.com>
2016-02-01 15:08:14 -0800

    methods

---
+++ javascript/theGoodParts/methods.js
@@ -0,0 +1,32 @@
+// Array
+array.concat(item...)
+
+array.join(separator)
+
+array.pop()
+
+array.push(item...)
+
+array.reverse()
+
+array.shift()
+
+array.slice(start, end)
+
+array.sort(comparefn)
+
+array.splice(start, deleteCount, item...)
+
+array.unshift(item...)
+
+// Function
+function.apply(thisArg, argArray)
+
+// Number
+number.toExponential(fractionDigits)
+
+number.toFixed(fractionDigits)
+
+number.toPrecision(precision)
+
+number.toString(radix)
