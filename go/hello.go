a6e221a703a040cf65705855b6187effe625000a
Keala Lusk <contactkeala@gmail.com>
2016-04-18 16:57:22 -0700

    arrays

--- go/hello.go
+++ go/hello.go
@@ -30,3 +30,46 @@ fmt.Println(sum)
   defer fmt.Println("cool")
     
   fmt.Println("this is")
+
+  fmt.Println("counting")
+
+  for i := 0; i < 10; i++ {
+    defer fmt.Println(i)
+  }
+
+fmt.Println("done")
+
+// pointers
+
+i, j := 42, 2701
+
+p := &i         // point to i
+fmt.Println(*p) // read i through the pointer
+*p = 21         // set i through the pointer
+fmt.Println(i)  // see the new value of i
+
+p = &j         // point to j
+*p = *p / 37   // divide j through the pointer
+fmt.Println(j) // see the new value of j
+
+
+// structs (accessed using a dot)
+type Vertex struct {
+  X int
+  Y int
+}
+
+func main() {
+  fmt.Println(Vertex{1, 2})
+}
+// --> {1 2}
+
+// arrays
+var a [2]string
+a[0] = "Hello"
+a[1] = "World"
+fmt.Println(a[0], a[1])
+fmt.Println(a)
+
+primes := [6]int{2, 3, 5, 7, 11, 13}
+fmt.Println(primes)
