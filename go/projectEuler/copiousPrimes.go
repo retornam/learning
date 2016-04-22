54705e95606ea4b9881a62a8f6f6b6412f6b2fac
Keala Lusk <contactkeala@gmail.com>
2016-04-22 15:06:15 -0700

    copious primes

---
+++ go/projectEuler/copiousPrimes.go
@@ -0,0 +1,19 @@
+// The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+// Find the sum of all the primes below two million.
+
+package main
+
+import(
+  "fmt"
+)
+
+func isPrime(n int) int {
+  var d int = 2
+  for n > d {
+  } 
+}
+
+func main() {
+  fmt.Println()
+}
